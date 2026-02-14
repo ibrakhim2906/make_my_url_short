47a07f0a2a2d8a5d7175f0c7f4afb4ee784f0458
Ibrakhim <ibrakhim2906@yahoo.com>
2026-02-14 11:13:58 +0500

    ...

---
+++ src/main/java/com/ibrakhim2906/makemyurlshort/MakemyurlshortApplication.java
@@ -0,0 +1,13 @@
+package com.ibrakhim2906.makemyurlshort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakemyurlshortApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakemyurlshortApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ibrakhim2906/makemyurlshort/MakemyurlshortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrakhim2906.makemyurlshort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakemyurlshortApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
